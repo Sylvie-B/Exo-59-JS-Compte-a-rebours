44f461ff03ca30c177528635ab4c403ab2acc754
Sylvie-B <unsolo@hotmail.fr>
2020-12-01 11:26:48 +0100

    avec retour à la ligne

--- appCompteARebours.js
+++ appCompteARebours.js
@@ -1,9 +1,6 @@
 /** recup du texte dans p */
 let text = document.getElementById("time")
-console.log(text);
-console.log(text.innerText);
 
 for (let t = 60; t >= 0; t--){
-    text.innerText += t.toString();
-    
+    text.innerHTML += "<br>" + t.toString();
 }
